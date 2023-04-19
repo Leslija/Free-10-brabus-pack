1cb9a35dc72c4ed29e5aea2c8e9985d36f6d9a67
Aku0073 <131307388+Aku0073@users.noreply.github.com>
2023-04-19 23:00:55 +0200

    Add files via upload

---
+++ brabuspack/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+game 'gta5'
+name "Brabus Pack by Dr.Disconnect#6969"
+description "Carpack featuring some BRABUS cars!"
+
+files {
+    --allcars
+    '**/vehiclelayouts.meta',
+    '**/vehicles.meta',
+    '**/carvariations.meta',
+    '**/carcols.meta',
+    '**/handling.meta',
+    '**/clips_sets.xml'
+}
+
+--allcars
+data_file 'HANDLING_FILE' '**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' '**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' '**/vehicles.meta'
+data_file 'CARCOLS_FILE' '**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '**/carvariations.meta'
